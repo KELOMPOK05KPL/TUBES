839cf9fb33d5d279c95e7cce35127bdfb429c1bc
Mohammad Ilham Firdaus <ilhamfir09@gmail.com>
2025-05-03 18:18:14 +0700

    feat: tambah json dan model vehicle

--- Tubes_API/Models/vehicle.cs
+++ Tubes_API/Models/vehicle.cs
@@ -1,17 +1,27 @@
 namespace Test_API_tubes.Models
 {
+    public enum VehicleState
+    {
+        Available,
+        Rented
+    }
     public class Vehicle
     {
-        //public int Id { get; set; }
-        //public string Brand { get; set; }
-        //public string Model { get; set; }
-        //public string Type { get; set; }
-        //public bool IsAvailable { get; set; }
-
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Type { get; set; } // e.g., Mobil, Motor
-        public bool IsAvailable { get; set; } = true;
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; } // e.g., Mobil, Motor
+        //public bool IsAvailable { get; set; } = true;
+        public VehicleState State { get; set; } = VehicleState.Available;
+
+        public Vehicle(int id, string type, string brand, string model, VehicleState state)
+        {
+            Id = id;
+            Type = type;
+            Brand = brand;
+            Model = model;
+            State = state;
+        }
     }
 
 }
