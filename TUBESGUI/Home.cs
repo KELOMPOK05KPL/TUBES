363b6f4cb080ffc1fb686e8c7028242307d99bac
Mohammad Ilham Firdaus <ilhamfir09@gmail.com>
2025-05-30 00:30:54 +0700

    feat: add home window

--- TUBESGUI/Home.cs
+++ TUBESGUI/Home.cs
@@ -16,5 +16,45 @@ namespace TUBESGUI
         {
             InitializeComponent();
         }
+
+        //Page home
+        private void BtnHome_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Home button clicked");
+        }
+
+        //Page All Vehicle
+        private void BtnAllVehicle_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("All Vehicle button clicked");
+        }
+
+        //Page Rent Vehicle
+        private void BtnRentVehicle_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Rent Vehicle button clicked");
+        }
+
+        //Page Return Vehicle
+        private void BtnReturnVehicle_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Return Vehicle button clicked");
+        }
+
+        //Page History
+        private void BtnHistory_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("History button clicked");
+        }
+
+        //Logout button
+        private void BtnLogout_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to logout?", "Logout", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                this.Close(); // atau tampilkan form login
+            }
+        }
     }
 }
